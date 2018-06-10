ba1f8dd445bf98fa430b9c4c83956c3db8ac191e
Angus McIntyre <angus@nomadcode.com>
2018-06-10 13:33:15 -0400

    Implement hash computation

--- lib/middleman-sri_calculator/extension.rb
+++ lib/middleman-sri_calculator/extension.rb
@@ -1,34 +1,46 @@
 # Require core library
 require 'middleman-core'
+require 'yaml'
 
 # Extension namespace
 class SRICalculator < ::Middleman::Extension
-  option :my_option, 'default', 'An example option'
+  option :paths, [], 'List of pathnames for which we need to calculate a hash'
+  option :datafile, 'sri.yaml', "Name of data file used to store SRI hashes"
+  option :template, "shasum -b -a 384 %s | xxd -r -p | base64", "Command template"
+  option :prefix, "sha384", "Identifier of hashing algorithm"
 
   def initialize(app, options_hash={}, &block)
-    # Call super to build options from the options_hash
     super
-
-    # Require libraries only when activated
-    # require 'necessary/library'
-
-    # set up your extension
-    # puts options.my_option
-    puts('Initialized')
   end
 
-  def after_configuration
-    puts('After configuration')
+  # after_build
+  #
+  # After the build is complete, refresh the SRI data file
+
+  def after_build(builder)
+  	datafilepath = File.join(builder.app.config[:data_dir], "/", options[:datafile])
+  	sri_data = {}
+	if File.exists?(datafilepath)
+  	  sri_data = YAML.load_file(datafilepath)
+  	end
+	options.paths.each { |path|
+		relative_path = File.join(builder.app.config[:build_dir], "/", path)
+		hash = compute_hash(relative_path)
+		normalized_name = path.gsub(/[^A-Za-z0-9]/,'_')
+		sri_data[normalized_name] = hash
+	}
+	File.write(datafilepath, sri_data.to_yaml)
   end
 
-  # A Sitemap Manipulator
-  # def manipulate_resource_list(resources)
-  # end
+  # compute_hash
+  #
+  # Compute a hash for the specified path by using a shell command.
 
-  # helpers do
-  #   def a_helper
-  #   end
-  # end
+  def compute_hash(path)
+  	command = options.template % path
+  	hash = `#{command}`.strip!
+  	return options.prefix + "-" + hash
+  end
+
 end
 
-::Middleman::Extensions.register(:sri_calculator, SRICalculator)
